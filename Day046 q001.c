cc9d59d221b5366a8989642484a4ec503db0c7fb
Anunay1826 <anunaymisra2006@gmail.com>
2026-03-29 23:46:08 +0530

    Create Day046 q001.c

---
+++ Day046 q001.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* newNode(int val) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    return n;
+}
+
+Node* buildTree(int* arr, int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    Node* queue[n];
+    int front = 0, rear = 0;
+
+    Node* root = newNode(arr[0]);
+    queue[rear++] = root;
+    int i = 1;
+
+    while (front < rear
